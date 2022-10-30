96c9002ef4b5fc4e128d30d2815131a54892867a
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-10-30 16:58:23 +0500

    head link added & styled

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -14,6 +14,9 @@ const Head = () => {
   return (
     <div className="head">
       <h1>Tic Tac Toe</h1>
+      <a href="https://github.com/VividTruthKeeper/react-tic-tac-toe">
+        GitHub: <span>VividTruthKeeper/react-tic-tac-toe</span>
+      </a>
       <div className="types">
         <TypeBtn
           onclick={() => setBoardType("three")}
